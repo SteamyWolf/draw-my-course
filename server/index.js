23188427e7a13b0b511a24a6ec4330126313dd13
Wyatt Allan <wyatt@site-source.com>
2022-11-03 21:45:59 -0600

    Added compression as per max on udemy and a procfile for the heroku server.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const compression = require("compression");
 const cartRoute = require("./routes/cart");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
@@ -13,11 +14,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-mongoose
-  .connect(process.env.MONGO_URL, { dbName: "draw-my-course" })
-  .then(() => console.log("DB Connection Successful"))
-  .catch((err) => console.log(err));
-
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use("/api/cart", cartRoute);
@@ -26,6 +23,12 @@ app.use("/api/order", orderRoute);
 app.use("/api/stripe", stripeRoute);
 app.use("/api/mail", nodemailerRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URL, { dbName: "draw-my-course" })
+  .then(() => {
+    console.log("DB Connection Successful");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
